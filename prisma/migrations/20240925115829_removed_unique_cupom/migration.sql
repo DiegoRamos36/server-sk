f6f017677e8f0085fa379b6317965d83e66f95db
Diego Ramos <diegoframos36@gmail.com>
2024-10-30 10:22:12 -0300

    first commit

--- prisma/migrations/20240925115829_removed_unique_cupom/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "cupons_codigo_key";
--- prisma/migrations/20241021165143_add_unique_username/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[username]` on the table `Funcionarios` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Funcionarios_username_key" ON "Funcionarios"("username");
