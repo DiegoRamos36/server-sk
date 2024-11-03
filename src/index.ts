25462834c1a991fd8ef95e7d466415721b2e7d66
Diego Ramos <diegoframos36@gmail.com>
2024-11-03 17:18:54 -0300

    port-changes-3

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 import { EmployeeRoutes } from './routes/employeeRoutes';
 
 dotenv.config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 10000;
 const app = fastify();
 app.register(fastifyJwt, {
   secret: process.env.JWT_KEY || '',
