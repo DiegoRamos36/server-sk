2d209bad23d27f5ed7dc18e33b9ddbc6b0a223f9
Diego Ramos <diegoframos36@gmail.com>
2025-01-10 12:30:45 -0300

    2.0.2

--- src/routes/branchRoutes.ts
+++ src/routes/branchRoutes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance, FastifyPluginCallback } from 'fastify';
-import { deleteBranch, newBranch } from '../controllers/branchController';
+import {
+  allBranches,
+  deleteBranch,
+  newBranch,
+} from '../controllers/branchController';
 
 export const BranchRoutes: FastifyPluginCallback = (app, options, done) => {
   app.post('/registrar-filial', newBranch);
   app.post('/deletar-filial', deleteBranch);
+  app.get('/ver-filial', allBranches);
 
   done();
 };
