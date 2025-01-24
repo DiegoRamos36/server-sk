74ceee5a1fbaff407a027bcc6e76b44c85bfdefe
Diego Ramos <diegoframos36@gmail.com>
2025-01-24 11:07:29 -0300

    fix-route

--- src/routes/branchRoutes.ts
+++ src/routes/branchRoutes.ts
@@ -10,7 +10,7 @@ export const BranchRoutes: FastifyPluginCallback = (app, options, done) => {
   app.post('/registrar-filial', newBranch);
   app.post('/deletar-filial', deleteBranch);
   app.get('/ver-filial', allBranches);
-  app.get('/ver-filial-id', branchById);
+  app.post('/ver-filial-id', branchById);
 
   done();
 };
